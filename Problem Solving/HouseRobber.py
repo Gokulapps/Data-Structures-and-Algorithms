f15dbdaa345d69ff0f42d4b80aea063e4e248d57
Gokulapps <61132761+Gokulapps@users.noreply.github.com>
2025-02-24 09:38:37 +0530

    Create HouseRobber.py

---
+++ Problem Solving/HouseRobber.py
@@ -0,0 +1,13 @@
+def rob(houses): 
+    rob1 = 0 
+    rob2 = 0
+    for house in houses: 
+        temp = max(rob1+house, rob2)
+        rob1 = rob2 
+        rob2 = temp 
+    
+    return rob2 
+    
+houses = [1, 4, 5, 2, 1, 6]
+max_amount = rob(houses)
+print('The Maximum Amount that can be robbed is: ', max_amount)
