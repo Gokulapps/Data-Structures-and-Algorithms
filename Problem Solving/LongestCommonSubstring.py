af7677d4dbc9eb2703b6d4a0b7790c35167e2401
Gokulapps <61132761+Gokulapps@users.noreply.github.com>
2025-03-09 11:45:08 +0530

    Create LongestCommonSubstring.py

---
+++ Problem Solving/LongestCommonSubstring.py
@@ -0,0 +1,18 @@
+# Dynamic Programming Algorithm 
+def lcs(text1, text2):
+    dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
+    res_text = ''
+    for i in range(len(text1)-1, -1, -1): 
+        for j in range(len(text2)-1, -1, -1):
+            if text1[i] == text2[j]: 
+                dp[i][j] = 1 + dp[i+1][j+1]
+                res_text = text1[i] + res_text
+            else:
+                dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+    
+    return dp[0][0], res_text
+
+text1 = "abcdefgh"
+text2 = "adg"
+result, seq = lcs(text1, text2)
+print('The Length of the Longest Common String Length is:', result, 'and the LCS is:', seq)
