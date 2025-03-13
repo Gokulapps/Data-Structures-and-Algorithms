db26abe775b284b0d6884786d723726da9277a82
Gokulapps <61132761+Gokulapps@users.noreply.github.com>
2025-03-13 10:58:28 +0530

    Update CoinChange2.py

--- Problem Solving/CoinChange2.py
+++ Problem Solving/CoinChange2.py
@@ -1,4 +1,27 @@
-# Algorithm 1 - Recursion Solution along with ensuring unique combinations
+# Algorithm 1 - Iterative Solution without cache but ensuring that only unique combinations are considered
+def coinComb(target, denominations):
+    noCoins = len(denominations)
+    noCombinations = 0
+    stack = [(0, 0)]
+    while stack: 
+        coin_index, total_amount = stack.pop(0)
+        if total_amount == target: 
+            noCombinations += 1
+        elif total_amount > target:
+            continue 
+        elif coin_index >= noCoins: 
+            continue
+        else: 
+            stack.append((coin_index, total_amount + denominations[coin_index]))
+            stack.append((coin_index + 1, total_amount))
+    
+    return noCombinations
+denominations = [1, 3, 5, 10, 20]
+amount = 5
+totalWays = coinComb(amount, denominations)
+print(f"Total No of Ways to sum up to the given amount {amount} is:", totalWays)
+
+# Algorithm 2 - Recursion Solution with cache along with ensuring unique combinations
 def coinComb(target, denominations):
     noCoins = len(denominations)
     cache = {}
@@ -23,25 +46,4 @@ amount = 500
 totalWays = coinComb(amount, denominations)
 print(f"Total No of Ways to sum up to the given amount {amount} is:", totalWays)
 
-# Algorithm 2 - Iterative Solution along with ensuring unique combinations
-def coinComb(target, denominations):
-    noCoins = len(denominations)
-    noCombinations = 0
-    stack = [(0, 0)]
-    while stack: 
-        coin_index, total_amount = stack.pop(0)
-        if total_amount == target: 
-            noCombinations += 1
-        elif total_amount > target:
-            continue 
-        elif coin_index >= noCoins: 
-            continue
-        else: 
-            stack.append((coin_index, total_amount + denominations[coin_index]))
-            stack.append((coin_index + 1, total_amount))
-    
-    return noCombinations
-denominations = [1, 3, 5, 10, 20]
-amount = 5
-totalWays = coinComb(amount, denominations)
-print(f"Total No of Ways to sum up to the given amount {amount} is:", totalWays)
+# Algorithm 3 - Dynamic Programming Solution 
